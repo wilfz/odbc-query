09b302d2803e78e5c3a61abbcb9c6891f194b7b3
wilfzim <48259856+wilfz@users.noreply.github.com>
2025-10-21 22:18:10 +0200

    renamed single_header to headeronly

--- headeronly.sh
+++ headeronly.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
+mkdir headeronly 
 cd query
-cat tstring.h std_includes.h > ../single_header/odbcquery.hpp
-cat odbcexception.h connection.h dbitem.h fieldinfo.h resultinfo.h datarow.h paraminfo.h paramitem.h query.h table.h lvstring.h odbcenvironment.h connection.cpp dbitem.cpp fieldinfo.cpp resultinfo.cpp datarow.cpp paramitem.cpp lvstring.cpp query.cpp odbcexception.cpp odbcenvironment.cpp table.cpp | grep -iv "#include" | grep -iv "#pragma once" >> ../single_header/odbcquery.hpp
+cat tstring.h std_includes.h > ../headeronly/odbcquery.hpp
+cat odbcexception.h connection.h dbitem.h fieldinfo.h resultinfo.h datarow.h paraminfo.h paramitem.h query.h table.h lvstring.h odbcenvironment.h connection.cpp dbitem.cpp fieldinfo.cpp resultinfo.cpp datarow.cpp paramitem.cpp lvstring.cpp query.cpp odbcexception.cpp odbcenvironment.cpp table.cpp | grep -iv "#include" | grep -iv "#pragma once" >> ../headeronly/odbcquery.hpp
 cd ..
